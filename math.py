5dfc17cfd4cbb6e1be25c83916dc25186af3bb29
A P Aishwarya Lakshmi <77623254+aishu19-dotcom@users.noreply.github.com>
2021-07-03 15:50:14 +0530

    Delete math.py

--- math.py
+++
@@ -1,6 +0,0 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> x = min(20,5,80)
-y = max(40,50,49)
-print(x)
-print(y)
