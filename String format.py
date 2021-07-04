dd1076d4e0b6bb711981b51729a4bdc2c45ae1b3
A P Aishwarya Lakshmi <77623254+aishu19-dotcom@users.noreply.github.com>
2021-07-04 21:04:19 +0530

    Add files via upload

---
+++ String format.py
@@ -0,0 +1,13 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #String Formatting
+>>> quantity=3
+>>> itemno=2
+>>> price=49
+>>> myorder= "I want {} pieces of item number {} for {:.5f} dollars."
+>>> print(myorder.format(quatity,itemno,price))
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print(myorder.format(quatity,itemno,price))
+NameError: name 'quatity' is not defined
+>>> 
