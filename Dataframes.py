596520350a3854a769488479489f08dd078d9ab9
A P Aishwarya Lakshmi <77623254+aishu19-dotcom@users.noreply.github.com>
2021-07-06 10:56:53 +0530

    -

--- Dataframes.py
+++ Dataframes.py
@@ -3,10 +3,5 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> #Panda Dataframe:
 >>> import pandas as pd
 >>> df= pd.read_csv('C:\Users\Aishwarya\Desktop\data.csv')
-SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 2-3: truncated \UXXXXXXXX escape
 >>> print(df)
-Traceback (most recent call last):
-  File "<pyshell#3>", line 1, in <module>
-    print(df)
-NameError: name 'df' is not defined
->>> 
+   
