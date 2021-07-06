23d55242ad1d6f7273b6a4fe1ef6ca391dc71347
A P Aishwarya Lakshmi <77623254+aishu19-dotcom@users.noreply.github.com>
2021-07-06 10:39:51 +0530

    -

---
+++ Panda Series.py
@@ -0,0 +1,11 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Panda Series:
+>>> import pandas as pd
+>>> books={"week1": "You Live Only Once","week2": "The Alchemist"}
+>>> myvar=pd.Series(books)
+>>> print(myvar)
+week1    You Live Only Once
+week2         The Alchemist
+dtype: object
+>>> 
