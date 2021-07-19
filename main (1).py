c94da742e67933cdd1346618c85d872299d79ac9
karthikarav <86820565+karthikarav@users.noreply.github.com>
2021-07-19 15:09:37 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,18 @@
+from numpy import random
+
+x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(100))
+
+print(x)
+
+from numpy import random
+import numpy as numpy
+arr = np.array([1,3,5,7,8])
+random.shuffle(arr)
+print(arr)
+
+from numpy import randam
+import numpy as np
+arr = np.array([1,3,5,6,7])
+a = random.permutation(arr)
+print(a)
+
