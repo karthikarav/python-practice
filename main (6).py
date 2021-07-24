9508cdea0a4241364562638348aaa953dd0dc461
karthikarav <86820565+karthikarav@users.noreply.github.com>
2021-07-24 14:10:48 +0530

    numpy ufuncs

---
+++ main (6).py
@@ -0,0 +1,30 @@
+import numpy as np
+arr = np.arange(1,20)
+print(np.log2(arr))
+
+import numpy as np
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([1, 2, 3])
+newarr = np.sum([arr1, arr2])
+print(newarr)
+
+import numpy as np
+arr1 = np.array([3,6,8])
+arr2 = np.array([5,7,9])
+newarr = np.sum([arr1,arr2])
+print(newarr)
+
+import numpy as np
+arr = np.array([3,6,8])
+newarr = np.cumprod([arr1,arr2])
+print(newarr)
+
+import numpy as np
+arr = np.arr([40,39,80,60])
+newarr = np.diff([arr1,arr2])
+print(newarr)
+
+
+
+
+
